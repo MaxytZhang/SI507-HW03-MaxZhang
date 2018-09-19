15981a4c317685049b3a1272e0951ac7acf02867
Yangtao(Max) Zhang <maxzhang@umich.edu>
2018-09-19 18:18:18 -0400

    A's first turn

---
+++ magic_eight.py
@@ -0,0 +1,11 @@
+def ask_question():
+    question = input("What is your question?: ")
+    while question[-1] != "?" and question != "quit":
+        question = input("I’m sorry, I can only answer questions.\nWhat is your question?: ")
+
+    return question
+
+
+while ask_question() != "quit":
+    if ask_question() == "quit":
+        break
